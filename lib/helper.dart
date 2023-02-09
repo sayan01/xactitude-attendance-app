c20abca7b7a5ec2ff69fcdfe80281f468e300f7e
Sayan Ghosh <sgic2001@gmail.com>
2023-02-10 00:52:58 +0530

    remove unused imports

--- lib/helper.dart
+++ lib/helper.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:csv/csv.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:ftest/main.dart';
 import 'package:path_provider/path_provider.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:prompt_dialog/prompt_dialog.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 // firebase
